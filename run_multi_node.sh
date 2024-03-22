0337d32859b5ed782a794de7054661391191b9c5
Lucas Nogueira <lucasn1@narval2.narval.calcul.quebec>
2024-03-22 17:57:03 -0400

    new branch with multi-node bench

---
+++ run_multi_node.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+BASE_DIR=$PWD
+
+export HF_HOME=$BASE_DIR/hf_cache
+export HF_DATASETS_CACHE=$BASE_DIR/hf_cache
+export TRANSFORMERS_CACHE=$BASE_DIR/hf_cache
+
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+
+
+MAIN_NODE=$1
+MAIN_PORT=$2
+N_NODES=$3
+N_GPUS=$4
+
+
+run_bench_parallel(){
+
+   cd $BASE_DIR/benchmarks/$1
+   echo "Running $1 benchmark with ${N_NODES} nodes and ${N_GPUS} device(s) per node..."
+
+   N_PROCS=$((${N_NODES} * ${N_GPUS}))
+
+       if [ "$1" == "large_language_model" ]; then
+
+          accelerate launch --mixed_precision=fp16 --num_machines=${N_NODES} --num_processes=${N_PROCS} --main_process_ip=${MAIN_NODE} --main_process_port=${MAIN_PORT} --config_file="${BASE_DIR}/configs/fsdp_llama.yaml"  main.py
+
+       else
+
+          accelerate launch --mixed_precision=fp16 --num_machines=${N_NODES} --num_processes=${N_PROCS} --main_process_ip=${MAIN_NODE} --main_process_port=${MAIN_PORT} main.py
+
+       fi
+       
+   done
+}
+
+
+echo "Starting benchmark suite..."
+
+BENCHMARKS=`ls $BASE_DIR/benchmarks`
+
+for bench in $BENCHMARKS
+do
+
+   run_bench $bench
+
+done
+
+cd $BASE_DIR
+
+echo "Collecting results and generating final performance report..."
+
+python $BASE_DIR/reporting/make_final_report.py
+
